f99fe95338eefaccf2a0ed815f826331a2ffbb7d
Ravi Chaudhary <RAVI.40813@stu.upes.ac.in>
2026-09-23 12:09:22 +0530

    Auto update - 100 Days of Code

---
+++ days/Day05/Q009.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float p, r, t, si, ci;
+
+    printf("Enter principal: ");
+    scanf("%f", &p);
+
+    printf("Enter rate: ");
+    scanf("%f", &r);
+
+    printf("Enter time: ");
+    scanf("%f", &t);
+
+    // Simple Interest
+    si = (p * r * t) / 100;
+
+    // Compound Interest
+    ci = p * pow((1 + r / 100), t) - p;
+
+    printf("Simple Interest = %.2f\n", si);
+    printf("Compound Interest = %.2f\n", ci);
+
+    return 0;
+}
